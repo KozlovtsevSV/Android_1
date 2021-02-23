b4a695e1f0dc2e620cb0401949196a95410efb90
Sergey <KozlovtsevSV@gmail.com>
2021-02-23 12:37:31 +0300

    HW#2

--- app/src/main/java/com/example/lesson_1/MainActivity.java
+++ app/src/main/java/com/example/lesson_1/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.lesson_1;
 
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,9 +9,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button buttonSesond = findViewById(R.id.button);
-        buttonSesond.setOnClickListener(view -> {setContentView(R.layout.second); });
+        setContentView(R.layout.keyboard_frame);
+        //Button buttonSesond = findViewById(R.id.button);
+        //buttonSesond.setOnClickListener(view -> {setContentView(R.layout.second); });
 
 
     }
